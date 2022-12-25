b9333fa7a558b39261ed097513c93b9c83101249
theanuragkumar <theanuragkumarofficial@gmail.com>
2022-12-25 09:37:30 +0530

    Updates

---
+++ precache-manifest.1405409b6ac72928591d31fcc0957cae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a4d3db9872b855af288ae3962dd7e7",
+    "url": "/theanuragkumar.github.io/index.html"
+  },
+  {
+    "revision": "e45c128f75f225e06bb4",
+    "url": "/theanuragkumar.github.io/static/css/main.3799542d.chunk.css"
+  },
+  {
+    "revision": "003a2f49a1f44452c3d1",
+    "url": "/theanuragkumar.github.io/static/js/2.0741cd04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/theanuragkumar.github.io/static/js/2.0741cd04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e45c128f75f225e06bb4",
+    "url": "/theanuragkumar.github.io/static/js/main.4c05d3f6.chunk.js"
+  },
+  {
+    "revision": "391fd303620a6565c26b",
+    "url": "/theanuragkumar.github.io/static/js/runtime-main.59fdba65.js"
+  }
+]);
