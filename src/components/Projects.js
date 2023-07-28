14b9735886a7b00145540c64f69b6336c9fd37d1
anurag-kumar-kr <108922662+anurag-kumar-kr@users.noreply.github.com>
2023-07-28 11:35:15 -0400

    Enhanced the UI

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,52 +13,53 @@ const Services = () => {
     text:"",
   });
   const [state] = React.useState([
+    
     {
       id: 1,
-      icon: <FaBlogger className="commonIcons" />,
-      heading: "Blog App",
-      link: "https://www.anlakshya.in/",
+      icon: <FaShoppingBag className="commonIcons" />,
+      heading: "Hawkeye - One Stop Shop",
+      link: "",
       text:
-        "developed a Anlakshya-Blog App using microservice architecture where Admin can maintain blog-writers and Blog-writers can login and perform CRUD operations with their blogs and many more...",
+        "Innovatively designed and built Hawkeye to centralize important merchandising-related data, resulting in a 40% reduction in data retrieval and analysis time.",
     },
     {
       id: 2,
+      icon: <FaBlogger className="commonIcons" />,
+      heading: "Anucodes - Blog App",
+      link: "https://www.anucodes.com/",
+      text:
+        "Developed AnuCodes-Blog, a powerful and scalable blogging application, utilizing the microservice architecture. The primary goal was to create a feature-rich platform where blog writers can showcase their talent, and administrators can efficiently manage the entire ecosystem",
+    },
+    {
+      id: 3,
       icon: <FaNewspaper className="commonIcons" />,
       heading: "News App",
       link: "https://github.com/theanuragkumar/news",
       text:
-        "developed a news app using HTML, CSS, JavaScript, BootStrap, React JS and News API where you can watch Top News Headlines,Business News, Sports News and many more... ",
-    },
-    {
-      id: 3,
-      icon: <FaAddressBook className="commonIcons" />,
-      heading: "iNoteManager",
-      link: "https://github.com/theanuragkumar/iNotebook",
-      text:
-        "developed a iNoteManager-Your Own Digital Notebook using  React JS and Node Js where You can login and then You can perform CRUD operation with your own notes."
+        "Developed a news app using HTML, CSS, JavaScript, BootStrap, React JS and News API where you can watch Top News Headlines,Business News, Sports News and many more... ",
     },
     {
       id: 4,
+      icon: <FaAddressBook className="commonIcons" />,
+      heading: "iNoteManager - Todo App",
+      link: "https://github.com/theanuragkumar/iNotebook",
+      text:
+        "Developed a iNoteManager-Your Own Digital Notebook using  React JS and Node Js where You can login and then You can perform CRUD operation with your own notes."
+    },
+    {
+      id: 5,
       icon: <FaCircleNotch className="commonIcons" />,
       heading: "Covid-19 Predictor",
       link: "https://github.com/theanuragkumar/Covid19Predictor",
       text:
-        "developed a Covid-19 Predictor using Flask and Machine Learning Algorithm where You can predict the probability of getting Infacted from Covid-19 after entering some medical values.",
-    },
-    {
-      id: 5,
-      icon: <FaShoppingBag className="commonIcons" />,
-      heading: "Shooping App",
-      link: "",
-      text:
-        "developed a Shooping APP using  SpringBoot and Thymleaf where You can login or create your own account and then You can buy or add items  into your cart.",
+        "Developed a Covid-19 Predictor using Flask and Machine Learning Algorithm where You can predict the probability of getting Infacted from Covid-19 after entering some medical values.",
     },
     {
       id: 6,
       icon: <FaAddressBook className="commonIcons" />,
       heading: "Competency Management System",
       text:
-        "Developing an end-to-end system for competency tracker for the associates/employees within TCS for 3 levels of user (Admin, Supervisors, Employee)",
+        "The Competency Tracker System efficiently manages and tracks competencies for TCS associates and employees. It caters to Admins, Supervisors, and Employees with tailored functionalities and access to competency-related data.",
     },
   ]);
   return (
@@ -80,8 +81,7 @@ const Services = () => {
                   <div className="services__box-header">{info.heading}</div>
                   <div className="services__box-p">{info.text}</div>
                   <div className="services__box-b">Know More</div>
-                </div>
-              
+                </div>         
                 </a>
               </div>
             ))}
